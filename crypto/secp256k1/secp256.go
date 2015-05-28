2bcc813c387756bf602d5641c49a3b5808a51ca2
Zach Ramsay <zach@coinculture.info>
2015-05-28 15:56:45 -0400

    use CSPRNG instead of mixed entropy

--- crypto/secp256k1/secp256.go
+++ crypto/secp256k1/secp256.go
@@ -22,7 +22,7 @@ import (
 	"errors"
 	"unsafe"
 
-	"github.com/ethereum/go-ethereum/crypto/randentropy"
+	randentropy "github.com/ethereum/go-ethereum/crypto/randentropy"
 )
 
 //#define USE_FIELD_5X64
@@ -57,7 +57,7 @@ func GenerateKeyPair() ([]byte, []byte) {
 	const seckey_len = 32
 
 	var pubkey []byte = make([]byte, pubkey_len)
-	var seckey []byte = randentropy.GetEntropyMixed(seckey_len)
+	var seckey []byte = randentropy.GetEntropyCSPRNG(seckey_len)
 
 	var pubkey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&pubkey[0]))
 	var seckey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&seckey[0]))
@@ -100,7 +100,7 @@ func GeneratePubKey(seckey []byte) ([]byte, error) {
 }
 
 func Sign(msg []byte, seckey []byte) ([]byte, error) {
-	nonce := randentropy.GetEntropyMixed(32)
+	nonce := randentropy.GetEntropyCSPRNG(32)
 
 	var sig []byte = make([]byte, 65)
 	var recid C.int
